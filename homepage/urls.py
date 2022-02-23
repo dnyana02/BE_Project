6a7d5edad4205b024027c00fc2a948df6ee4c9e6
Dnyanesh Mahajan <dnyanesh_mahajan@persistent.com>
2022-02-23 18:35:43 +0530

    Adding Django Route

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,11 @@
 from django import views
 from django.urls import path
-from . import views
+from homepage import views
 urlpatterns=[
     path('',views.index,name='index'),
+    path('forgetpass/',views.forgetpass,name='forgetpass'),
+    path('Signup/',views.Signup,name='Signup'),
+    path('home/',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('video/',views.video,name='video')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def index(request):
+    return render(request,'Login.html')
+
+def Signup(request):
+    return render(request,'SignUp.html')
+
+def forgetpass(request):
+    return render(request,'ForgetPass.html')
+
+def home(request):
     return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def video(request):
+    return render(request,'video.html')
