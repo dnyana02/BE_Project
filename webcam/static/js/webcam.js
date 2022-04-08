b8ddde0a22b4a560db7eba50a5ddf3f6203acae1
Dnyanesh Mahajan <dnyanesh_mahajan@persistent.com>
2022-04-08 09:52:42 +0530

    Video Stream to image conversion

--- webcam/static/js/webcam.js
+++ webcam/static/js/webcam.js
@@ -1,4 +1,4 @@
-console.log("Hello World!")
+
 //Initializing firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDVr9y6YoHToa8xPu-XRTtdVcZ2mNDUXus",
@@ -11,6 +11,7 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 // console.log(firebase);
 const firestore = firebase.firestore();
+console.log("Hello World!")
 // const callDoc = firestore.collection('calls').doc();
 // window.onload = get_cust_id;
 
@@ -42,9 +43,119 @@ const servers = {
     iceCandidatePoolSize: 10,
   };
   const remoteVideo = document.getElementById('remoteVideo');
+  const video = document.getElementById('localVideo');
   const localVideo = document.getElementById('localVideo');
 const pc = new RTCPeerConnection(servers);
-localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+localStream =  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function(stream) {
+  // Link to the video source
+  localVideo.srcObject = stream;
+  // Play video
+  localVideo.play();
+  console.log('photoButton');
+})
+.catch(function(err) {
+  console.log(`Error: ${err}`);
+});
+
+
+let width = 500,
+height = 0,
+filter = 'none',
+streaming = false;
+
+// DOM Elements
+
+const canvas = document.getElementById('canvas');
+const photos = document.getElementById('photos');
+const photoButton = document.getElementById('photo-button');
+const clearButton = document.getElementById('clear-button');
+const photoFilter = document.getElementById('photo-filter');
+
+
+
+  // Play when ready
+  video.addEventListener('canplay', function(e) {
+    if(!streaming) {
+      // Set video / canvas height
+      height = video.videoHeight / (video.videoWidth / width);
+
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+
+      streaming = true;
+    }
+  }, false);
+
+ // Photo button event
+ photoButton.addEventListener('click', function(e) {
+   console.log('photoButton');
+  takePicture();
+
+  e.preventDefault();
+}, false);
+
+// Filter event
+photoFilter.addEventListener('change', function(e) {
+  // Set filter to chosen option
+  filter = e.target.value;
+  // Set filter to video
+  video.style.filter = filter;
+
+  e.preventDefault(); 
+});
+
+// Clear event
+clearButton.addEventListener('click', function(e) {
+  // Clear photos
+  photos.innerHTML = '';
+  // Change filter back to none
+  filter = 'none';
+  // Set video filter
+  video.style.filter = filter;
+  // Reset select list
+  photoFilter.selectedIndex = 0;
+});
+
+// Take picture from canvas
+function takePicture() {
+  // Create canvas
+  console.log("take picture from canvas");
+  const context = canvas.getContext('2d');
+  if(width && height) {
+    // set canvas props
+    canvas.width = width;
+    canvas.height = height;
+    // Draw an image of the video on the canvas
+    context.drawImage(video, 0, 0, width, height);
+
+    // Create image from the canvas
+    const imgUrl = canvas.toDataURL('image/png');
+    downloadImage(imgUrl, 'my-canvas.jpeg');
+    // Create img element
+    const img = document.createElement('img');
+
+    // Set img src
+    img.setAttribute('src', imgUrl);
+
+    // Set image filter
+    img.style.filter = filter;
+
+    // Add image to photos
+    photos.appendChild(img);
+    
+  }
+}
+
+function downloadImage(data, filename = 'untitled.jpeg') {
+  var a = document.createElement('a');
+  a.href = data;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+}
+
   remoteStream = new MediaStream();
 const action = async() => {
 
@@ -71,6 +182,7 @@ const action = async() => {
 //Listen for remote answer
     callDoc.onSnapshot((snapshot) => {
         const data = snapshot.data();
+        
     console.log(data);
     
         if (!pc.currentRemoteDescription && data?.answer) {
@@ -130,6 +242,3 @@ const action = async() => {
 }
 action();
 action1();
-//Answer the call of the candidate
-
-  
